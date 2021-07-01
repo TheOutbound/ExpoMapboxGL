c202c96b99f22284814f823950549f1e801fb453
Ty Rauber <tyrauber@mac.com>
2021-07-01 11:22:12 -0400

    Add expo-location

--- App.js
+++ App.js
@@ -2,8 +2,27 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import MapboxGL from './MapboxGL';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
   return (
     <View style={{flex: 1}}>
       <MapboxGL.MapView style={{flex: 1}}>
